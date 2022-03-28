41e9048846ef8d2936cd5d215faf3df873b600ba
AfanasenkoYulka <afanasenkoyulka94@gmail.com>
2022-03-28 18:37:24 +0300

    add script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let a = 5;
-console.log(a);
+const arrows = document.querySelectorAll(".arrow");
+const redSlider = document.querySelector(".slider--red");
+const blueSlider = document.querySelector(".slider--blue");
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    if (redSlider.classList.contains("sliderHidden")) {
+      redSlider.classList.remove("sliderHidden");
+      blueSlider.classList.add("sliderHidden");
+    } else if (blueSlider.classList.contains("sliderHidden")) {
+      redSlider.classList.add("sliderHidden");
+      blueSlider.classList.remove("sliderHidden");
+    }
+  });
+});
